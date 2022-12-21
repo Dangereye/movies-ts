cb71d821c33033f55779b2507ef1598494858552
Craig Puxty <craig_puxty@talktalk.net>
2022-12-21 22:01:21 +0000

    Added trending people and tv shows > landing page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -14,6 +14,8 @@ import { IMovie } from "../interfaces/IMovie";
 
 // Utilities
 import { formatDate } from "../utilities/formatDate";
+import { IPerson } from "../interfaces/IPerson";
+import { ITVShow } from "../interfaces/ITVShow";
 
 const fetchData = async (endpoint: string) => {
   const res = await fetch(
@@ -25,27 +27,46 @@ const fetchData = async (endpoint: string) => {
 export default function LandingPage() {
   const {
     data: movies,
-    isLoading,
-    isError,
+    isLoading: moviesIsLoading,
+    isError: moviesIsError,
   } = useQuery({
     queryKey: ["trending movies"],
     queryFn: () => fetchData("trending/movie/week"),
   });
 
-  if (isLoading) {
-    return <H2 heading="loading" />;
+  const {
+    data: people,
+    isLoading: peopleIsLoading,
+    isError: peopleIsError,
+  } = useQuery({
+    queryKey: ["trending people"],
+    queryFn: () => fetchData("trending/person/week"),
+  });
+
+  const {
+    data: tvshows,
+    isLoading: tvshowsIsLoading,
+    isError: tvshowsIsError,
+  } = useQuery({
+    queryKey: ["trending tv shows"],
+    queryFn: () => fetchData("trending/tv/week"),
+  });
+
+  if (moviesIsLoading || peopleIsLoading || tvshowsIsLoading) {
+    return <H2 heading="Loading" />;
   }
 
-  if (isError) {
+  if (moviesIsError || peopleIsError || tvshowsIsError) {
     return <H2 heading="Error" />;
   }
 
   return (
     <>
       <Main>
+        {/* Trending Movies */}
         <Article>
           <Container>
-            <H2 heading="Movies trending this week" />
+            <H2 heading="trending movies" />
             <Cards
               getID={(item: IMovie) => item.id}
               renderLink={(item) => `/movies/${item.id}`}
@@ -63,6 +84,56 @@ export default function LandingPage() {
                 </>
               )}
               data={movies.results}
+            />
+          </Container>
+        </Article>
+
+        {/* Trending People */}
+        <Article>
+          <Container>
+            <H2 heading="trending people" />
+            <Cards
+              getID={(item: IPerson) => item.id}
+              renderLink={(item) => `/person/${item.id}`}
+              renderItem={(item: IPerson) => (
+                <>
+                  <ImageComponent
+                    src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`}
+                    fallback="/images/error_500x750.webp"
+                    alt={item.name}
+                  />
+                  <CardContent
+                    heading={item.name}
+                    body={item.known_for_department}
+                  />
+                </>
+              )}
+              data={people.results}
+            />
+          </Container>
+        </Article>
+
+        {/* Trending TV Shows */}
+        <Article>
+          <Container>
+            <H2 heading="trending tv shows" />
+            <Cards
+              getID={(item: ITVShow) => item.id}
+              renderLink={(item) => `/tv/${item.id}`}
+              renderItem={(item: ITVShow) => (
+                <>
+                  <ImageComponent
+                    src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                    fallback="/images/error_500x750.webp"
+                    alt={item.name}
+                  />
+                  <CardContent
+                    heading={item.name}
+                    body={formatDate(item.first_air_date)}
+                  />
+                </>
+              )}
+              data={tvshows.results}
             />
           </Container>
         </Article>
