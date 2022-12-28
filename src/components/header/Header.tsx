e9d299fb414643d41a2d75c037c4acbbd49d047a
Craig Puxty <craig_puxty@talktalk.net>
2022-12-28 14:32:07 +0000

    Added Header component.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+type HeaderProps = {
+  variant?: string;
+  children: ReactNode;
+};
+
+export default function Header({ variant, children }: HeaderProps) {
+  return <header className={`header ${variant}`}>{children}</header>;
+}
