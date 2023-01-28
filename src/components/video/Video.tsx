929e792ea5e4495a71740e9f8d4ed28437f5f82a
Craig Puxty <craig_puxty@talktalk.net>
2023-01-28 23:03:23 +0000

    Added video play icon, updated video styles.

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -3,6 +3,7 @@ import { formatDate } from "../../utilities/formatDate";
 import ImageComponent from "../image/Image";
 import BodyText from "../typography/BodyText";
 import HDiv from "../typography/HDiv";
+import { GoPlay } from "react-icons/go";
 
 type VideoProps = {
   data: IVideo;
@@ -11,14 +12,20 @@ type VideoProps = {
 export default function Video({ data }: VideoProps) {
   return (
     <div className="video">
-      <ImageComponent
-        width={500}
-        src={`https://i.ytimg.com/vi/${data.key}/hqdefault.jpg`}
-        fallback="/images/error_600x450.webp"
-        alt={data.name}
-      />
-      <HDiv variant="heading--h4" heading={data.name} />
-      <BodyText text={formatDate(data.published_at)} />
+      <div className="video-image">
+        <ImageComponent
+          width={500}
+          src={`https://i.ytimg.com/vi/${data.key}/hqdefault.jpg`}
+          fallback="/images/error_600x450.webp"
+          alt={data.name}
+        />
+        <div className="play-icon__bg"></div>
+        <GoPlay className="play-icon" />
+      </div>
+      <div className="content">
+        <HDiv variant="heading--h4" heading={data.name} />
+        <BodyText text={formatDate(data.published_at)} />
+      </div>
     </div>
   );
 }
