281e7e7d4c212b99f12bfa12daf9f3929a853449
Craig Puxty <craig_puxty@talktalk.net>
2023-02-04 22:46:25 +0000

    Updated ArticleVideos - display only if videos are available.

--- src/components/articles/ArticleVideos.tsx
+++ src/components/articles/ArticleVideos.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { IVideo } from "../../interfaces/IVideo";
+
+// Components
 import Button from "../buttons/Button";
 import Container from "../container/Container";
 import BodyText from "../typography/BodyText";
@@ -7,6 +8,9 @@ import H2 from "../typography/H2";
 import Video from "../videos/video/Video";
 import Wrapper from "../wrapper/Wrapper";
 import Article from "./Article";
+
+// Interfaces
+import { IVideo } from "../../interfaces/IVideo";
 
 type ActiveProps =
   | "trailer"
@@ -97,105 +101,111 @@ export default function ArticleVideos({ data }: VideoArticleProps) {
   const updateVideos = (value: ActiveProps) => {
     setActive(value);
   };
-
-  return (
-    <Article name="article__videos">
-      <Container>
-        <H2 heading={`Movie ${makePlural(active)}`} />
-        <Wrapper name="video-options" variant="flex">
-          {videos.trailer.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Trailers</span>
-                  <span className="qty">{videos.trailer.length}</span>
-                </>
-              }
-              active={active === "trailer"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("trailer")}
-            />
-          )}
-          {videos.teaser.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Teasers</span>
-                  <span className="qty">{videos.teaser.length}</span>
-                </>
-              }
-              active={active === "teaser"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("teaser")}
-            />
-          )}
-          {videos.clip.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Clips</span>
-                  <span className="qty">{videos.clip.length}</span>
-                </>
-              }
-              active={active === "clip"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("clip")}
-            />
-          )}
-          {videos.behind_the_scenes.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Behind the Scenes</span>
-                  <span className="qty">{videos.behind_the_scenes.length}</span>
-                </>
-              }
-              active={active === "behind_the_scenes"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("behind_the_scenes")}
-            />
-          )}
-          {videos.blooper.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Bloopers</span>
-                  <span className="qty">{videos.blooper.length}</span>
-                </>
-              }
-              active={active === "blooper"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("blooper")}
-            />
-          )}
-          {videos.featurette.length > 0 && (
-            <Button
-              name={
-                <>
-                  <span className="name">Featurettes</span>
-                  <span className="qty">{videos.featurette.length}</span>
-                </>
-              }
-              active={active === "featurette"}
-              variant="btn--tertiary"
-              onClick={(e) => updateVideos("featurette")}
-            />
-          )}
-        </Wrapper>
-        <div className="videos">
-          {videos[active].length > 0 ? (
-            videos[active]
-              .sort(function (a, b) {
-                return Date.parse(a.published_at) - Date.parse(b.published_at);
-              })
-              .map((video) => <Video key={video.id} data={video} />)
-          ) : (
-            <BodyText
-              text={`We don't seem to have any ${active} videos at this time.`}
-            />
-          )}
-        </div>
-      </Container>
-    </Article>
-  );
+  if (data && data.length > 0) {
+    return (
+      <Article name="article__videos">
+        <Container>
+          <H2 heading={`Movie ${makePlural(active)}`} />
+          <Wrapper name="video-options" variant="flex">
+            {videos.trailer.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Trailers</span>
+                    <span className="qty">{videos.trailer.length}</span>
+                  </>
+                }
+                active={active === "trailer"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("trailer")}
+              />
+            )}
+            {videos.teaser.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Teasers</span>
+                    <span className="qty">{videos.teaser.length}</span>
+                  </>
+                }
+                active={active === "teaser"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("teaser")}
+              />
+            )}
+            {videos.clip.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Clips</span>
+                    <span className="qty">{videos.clip.length}</span>
+                  </>
+                }
+                active={active === "clip"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("clip")}
+              />
+            )}
+            {videos.behind_the_scenes.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Behind the Scenes</span>
+                    <span className="qty">
+                      {videos.behind_the_scenes.length}
+                    </span>
+                  </>
+                }
+                active={active === "behind_the_scenes"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("behind_the_scenes")}
+              />
+            )}
+            {videos.blooper.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Bloopers</span>
+                    <span className="qty">{videos.blooper.length}</span>
+                  </>
+                }
+                active={active === "blooper"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("blooper")}
+              />
+            )}
+            {videos.featurette.length > 0 && (
+              <Button
+                name={
+                  <>
+                    <span className="name">Featurettes</span>
+                    <span className="qty">{videos.featurette.length}</span>
+                  </>
+                }
+                active={active === "featurette"}
+                variant="btn--tertiary"
+                onClick={(e) => updateVideos("featurette")}
+              />
+            )}
+          </Wrapper>
+          <div className="videos">
+            {videos[active].length > 0 ? (
+              videos[active]
+                .sort(function (a, b) {
+                  return (
+                    Date.parse(a.published_at) - Date.parse(b.published_at)
+                  );
+                })
+                .map((video) => <Video key={video.id} data={video} />)
+            ) : (
+              <BodyText
+                text={`We don't seem to have any ${active} videos at this time.`}
+              />
+            )}
+          </div>
+        </Container>
+      </Article>
+    );
+  }
+  return null;
 }
