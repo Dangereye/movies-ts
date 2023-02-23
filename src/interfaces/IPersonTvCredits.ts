2c5a18f97f39a69bd436559b40ac3315180a5e70
Craig Puxty <craig_puxty@talktalk.net>
2023-02-23 21:45:53 +0000

    Added IPersonTvCrew interface, updated IpersonTvCredits.

--- src/interfaces/IPersonTvCredits.ts
+++ src/interfaces/IPersonTvCredits.ts
@@ -1,7 +1,8 @@
 import { IPersonTvCast } from "./IPersonTvCast";
+import { IPersonTvCrew } from "./IPersonTvCrew";
 
 export interface IPersonTvCredits {
   id: number;
   cast: IPersonTvCast[];
-  crew: {}[];
+  crew: IPersonTvCrew[];
 }
