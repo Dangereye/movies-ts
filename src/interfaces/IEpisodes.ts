266a7ee8e0481036f1825bf91734685d8c0ee09c
Craig Puxty <craig_puxty@talktalk.net>
2023-02-27 22:14:37 +0000

    Added episodes to TvSeason.

--- src/interfaces/IEpisodes.ts
+++ src/interfaces/IEpisodes.ts
@@ -31,6 +31,7 @@ export interface IEpisodes {
   name: string;
   overview: string;
   production_code: string;
+  runtime: number;
   season_number: number;
   still_path: string;
   vote_average: number;
--- src/pages/TvSeason.tsx
+++ src/pages/TvSeason.tsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
+import Article from "../components/articles/Article";
+import Container from "../components/container/Container";
 import CrewJobs from "../components/header/CrewJobs";
 import Header from "../components/header/Header";
 import Overview from "../components/header/Overview";
+import ImageComponent from "../components/image/Image";
 import Navigation from "../components/navigation/Navigation";
+import StarRating from "../components/star_rating/StarRating";
 import SubNavbar from "../components/sub_navbar/SubNavbar";
 import BodyText from "../components/typography/BodyText";
 import H2 from "../components/typography/H2";
 import HDiv from "../components/typography/HDiv";
+import SmallText from "../components/typography/SmallText";
 import VoteCountPercentage from "../components/vote_count_percentage/VoteCountPercentage";
 import Wrapper from "../components/wrapper/Wrapper";
 import { tvPages } from "../data/tvPages";
@@ -14,6 +19,7 @@ import useMakeQuery from "../hooks/useMakeQuery";
 import { ISeason } from "../interfaces/ISeason";
 import { ITVShowFull } from "../interfaces/ITVShowFull";
 import { formatDate } from "../utilities/formatDate";
+import { formatRuntime } from "../utilities/formatRuntime";
 
 export default function TvSeason() {
   const { tvId, seasonId } = useParams();
@@ -73,6 +79,37 @@ export default function TvSeason() {
         <Overview text={season?.overview} />
         <CrewJobs credits={season?.credits} />
       </Header>
+      <Article name="season-episodes">
+        <Container>
+          <H2 heading="Episodes" />
+          <div className="episodes">
+            {season?.episodes.map((episode) => (
+              <div className="episode">
+                <ImageComponent
+                  src={`https://image.tmdb.org/t/p/w500/${episode.still_path}`}
+                  alt={episode.name}
+                  fallback="/images/error_600x450.webp"
+                />
+                <div className="content">
+                  <Wrapper name="episode-header" variant="flex">
+                    <HDiv
+                      variant="heading--h4"
+                      heading={`${episode.episode_number}. ${episode.name}`}
+                    />
+                    <BodyText text={formatRuntime(episode.runtime)} />
+                  </Wrapper>
+                  <SmallText text={formatDate(episode.air_date)} />
+                  <BodyText text={episode.overview} />
+                  <Wrapper name="episode-votes" variant="flex">
+                    <StarRating rating={episode.vote_average} />
+                    <SmallText text={`${episode.vote_count} votes`} />
+                  </Wrapper>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Container>
+      </Article>
     </>
   );
 }
