eecee9f9e0ae89802a0eb98a30443c646989950c
Craig Puxty <craig_puxty@talktalk.net>
2023-03-10 19:58:23 +0000

    Updated Review component and styles.

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,6 +1,5 @@
 // Components
 import ImageComponent from "../image/Image";
-import H3 from "../typography/H3";
 import SmallText from "../typography/SmallText";
 import Wrapper from "../wrapper/Wrapper";
 import StarRating from "../star_rating/StarRating";
@@ -20,24 +19,29 @@ type ReviewProps = {
 export default function Review({ data }: ReviewProps) {
   return (
     <div className="review">
-      <div className="avatar">
-        <Wrapper name="review-heading" variant="flex">
-          <HDiv variant="heading--h4" heading={`${data.author}`} />
-        </Wrapper>
-      </div>
-      <div className="content">
-        <div className="rating">
-          <StarRating rating={data.author_details.rating} />
+      <Wrapper name="review-heading" variant="flex">
+        <div className="avatar">
+          <ImageComponent
+            src={`https://image.tmdb.org/t/p/w500/${data.author_details.avatar_path}`}
+            fallback="/images/error_100x100.webp"
+            alt={data.author}
+            width={60}
+            height={60}
+          />
         </div>
-        <SmallText
-          text={
-            data.updated_at
-              ? `Updated ${formatDate(data.updated_at)}`
-              : `Created ${formatDate(data.created_at)}`
-          }
-        />
-        <ExpandableText text={data.content} lines={3} />
-      </div>
+        <div>
+          <HDiv variant="heading--h3" heading={`${data.author}`} />
+          <SmallText
+            text={
+              data.updated_at
+                ? formatDate(data.updated_at)
+                : formatDate(data.created_at)
+            }
+          />
+        </div>
+      </Wrapper>
+      <StarRating rating={data.author_details.rating} />
+      <ExpandableText text={data.content} lines={3} />
     </div>
   );
 }
