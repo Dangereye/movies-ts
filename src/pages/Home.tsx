1856240cd595293eb02b8cef645a730cf029ed58
Craig Puxty <craig_puxty@talktalk.net>
2023-04-01 21:50:05 +0100

    Updated MoviesPopular.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,58 +1,70 @@
 // Components
-import Main from "../components/main/Main";
-import H2 from "../components/typography/H2";
+import Main from '../components/main/Main';
+import H2 from '../components/typography/H2';
 
 // Interfaces
-import { IPage } from "../interfaces/IPage";
-import { IMovieMin } from "../interfaces/IMovieMin";
-import { IPerson } from "../interfaces/IPerson";
-import { ITVShowMin } from "../interfaces/ITVShowMin";
+import { IPage } from '../interfaces/IPage';
+import { IMovieMin } from '../interfaces/IMovieMin';
+import { IPerson } from '../interfaces/IPerson';
+import { ITVShowMin } from '../interfaces/ITVShowMin';
 
 // Hooks
-import useMakeQuery from "../hooks/useMakeQuery";
+import useMakeQuery from '../hooks/useMakeQuery';
 
 // Articles
-import ArticleMoviesMin from "../components/articles/ArticleMoviesMin";
-import ArticlePeople from "../components/articles/ArticlePeople";
-import ArticleTvMin from "../components/articles/ArticleTvMin";
+import ArticleMoviesMin from '../components/articles/ArticleMoviesMin';
+import ArticlePeople from '../components/articles/ArticlePeople';
+import ArticleTvMin from '../components/articles/ArticleTvMin';
 
 export default function LandingPage() {
   const {
     data: movies,
     isLoading: moviesIsLoading,
     isError: moviesIsError,
-  } = useMakeQuery<IPage<IMovieMin>>("trending movies", "trending/movie/week");
+  } = useMakeQuery<IPage<IMovieMin>>('trending movies', 'trending/movie/week');
 
   const {
     data: people,
     isLoading: peopleIsLoading,
     isError: peopleIsError,
-  } = useMakeQuery<IPage<IPerson>>("trending people", "trending/person/week");
+  } = useMakeQuery<IPage<IPerson>>('trending people', 'trending/person/week');
 
   const {
     data: tvshows,
     isLoading: tvshowsIsLoading,
     isError: tvshowsIsError,
-  } = useMakeQuery<IPage<ITVShowMin>>("trending tvshows", "trending/tv/week");
+  } = useMakeQuery<IPage<ITVShowMin>>('trending tvshows', 'trending/tv/week');
 
   if (moviesIsLoading || peopleIsLoading || tvshowsIsLoading) {
-    return <H2 heading="Loading" />;
+    return <H2 heading='Loading' />;
   }
 
   if (moviesIsError || peopleIsError || tvshowsIsError) {
-    return <H2 heading="Error" />;
+    return <H2 heading='Error' />;
   }
 
   return (
     <>
       <Main>
-        
-        <ArticleMoviesMin variant="scroll-x" name="trending-movies" heading="Trending movies" data={movies?.results}/>
-        <ArticlePeople variant="scroll-x" name="trending-people"heading="Trending people"data={people?.results}department/>
-<ArticleTvMin variant="scroll-x" name="trending-tv-shows"heading="Trending TV shows"data={tvshows?.results}/>
-        
-
-        
+        <ArticleMoviesMin
+          variant='scroll-x'
+          name='trending-movies'
+          heading='Trending movies'
+          data={movies?.results}
+        />
+        <ArticlePeople
+          variant='scroll-x'
+          name='trending-people'
+          heading='Trending people'
+          data={people?.results}
+          department
+        />
+        <ArticleTvMin
+          variant='scroll-x'
+          name='trending-tv-shows'
+          heading='Trending TV shows'
+          data={tvshows?.results}
+        />
       </Main>
     </>
   );
--- src/pages/MoviesPopular.tsx
+++ src/pages/MoviesPopular.tsx
@@ -1,3 +1,51 @@
+import Article from '../components/articles/Article';
+import Container from '../components/container/Container';
+import Layout from '../components/layout/Layout';
+import Main from '../components/main/Main';
+import Navigation from '../components/navigation/Navigation';
+import Sidebar from '../components/sidebar/Sidebar';
+import SubNavbar from '../components/sub_navbar/SubNavbar';
+import H2 from '../components/typography/H2';
+import { moviePages } from '../data/moviePages';
+import useMakeQuery from '../hooks/useMakeQuery';
+import { IMovieMin } from '../interfaces/IMovieMin';
+import { IPage } from '../interfaces/IPage';
+
 export default function MoviesPopular() {
-  return <>Popular Movies</>;
+  const {
+    data: movies,
+    isLoading,
+    isError,
+  } = useMakeQuery<IPage<IMovieMin>>('popular movies', '/movie/popular');
+
+  if (isLoading) {
+    return <H2 heading='Loading' />;
+  }
+
+  if (isError) {
+    return <H2 heading='Error' />;
+  }
+
+  return (
+    <>
+      <SubNavbar>
+        <Navigation
+          data={moviePages}
+          getID={(item) => item.name}
+          getLink={(item) => item.link}
+          renderItem={(item) => item.name}
+          variant='horizontal'
+        />
+      </SubNavbar>
+      <Article name='popular-movies'>
+        <Container>
+          <H2 heading='Popular Movies' />
+          <Layout variant='grid grid--sidebar'>
+            <Sidebar>sidebar</Sidebar>
+            <Main>main</Main>
+          </Layout>
+        </Container>
+      </Article>
+    </>
+  );
 }
