ed59dbb1d1f79ff5b55c1eede9735e8b9a86336b
Craig Puxty <craig_puxty@talktalk.net>
2023-04-30 21:18:39 +0100

    Updated useMakeInfiniteQuery.

--- src/hooks/useMakeInfiniteQuery.tsx
+++ src/hooks/useMakeInfiniteQuery.tsx
@@ -1,8 +1,9 @@
 import { QueryFunctionContext, useInfiniteQuery } from '@tanstack/react-query';
 
 const fetchData = async ({ queryKey, pageParam = 1 }: QueryFunctionContext) => {
+  const [endPoint, append] = queryKey;
   const res = await fetch(
-    `https://api.themoviedb.org/3/${queryKey[0]}?api_key=2f965fedbcdec55779f7e2e60eb62e59&page=${pageParam}${queryKey[1]}`
+    `https://api.themoviedb.org/3/${endPoint}?api_key=2f965fedbcdec55779f7e2e60eb62e59&page=${pageParam}${append}`
   );
   return res.json();
 };
