5b16afb0a6e004f31b26879fdc1a5af665dd00c9
Craig Puxty <craig_puxty@talktalk.net>
2023-05-30 12:37:02 +0100

    Updated useAppendTv.

--- src/hooks/useAppendTv.tsx
+++ src/hooks/useAppendTv.tsx
@@ -5,10 +5,18 @@ import { TvFiltersContext } from '../contexts/TvFiltersContext';
 
 export default function useAppendTv() {
   const { state } = useContext(TvFiltersContext);
-  const append = `&sort_by=${state.sort}&watch_region=${
-    state.region
-  }&with_watch_monetization_types=${state.release_types
-    .toString()
-    .replaceAll(',', '|')}`;
+  const append = `&sort_by=${state.sort}&include_adult=${
+    state.adult
+  }&watch_region=${state.region}${
+    state.release_types.length
+      ? `&with_watch_monetization_types=${state.release_types
+          .toString()
+          .replaceAll(',', '|')}`
+      : ''
+  }${state.date_from ? `&first_air_date.gte=${state.date_from}` : ''}${
+    state.date_to ? `&first_air_date.lte=${state.date_to}` : ''
+  }${state.genres.length ? `&with_genres=${state.genres}` : ''}${
+    state.vote_count ? `&vote_count.gte=${state.vote_count}` : ''
+  }`;
   return { append };
 }
