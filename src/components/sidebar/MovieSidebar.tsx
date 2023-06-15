8944fdc3b0663002ceb4982a2d4bb477102b02a5
Craig Puxty <craig_puxty@talktalk.net>
2023-06-15 19:59:03 +0100

    Added useMovieFilterFunctions hook. Updated MovieSidebar.

--- src/components/sidebar/MovieSidebar.tsx
+++ src/components/sidebar/MovieSidebar.tsx
@@ -5,6 +5,7 @@ import { MovieFiltersContext } from '../../contexts/MovieFiltersContext';
 
 // Hooks
 import useCreateMovieGenres from '../../hooks/useCreateMovieGenres';
+import useMovieFilterFuntions from '../../hooks/useMovieFilterFunctions';
 
 // Components
 import Section from './sections/Section';
@@ -18,245 +19,29 @@ import { movieSortOptions } from '../../data/movieSortOptions';
 import { movieReleaseTypes } from '../../data/movieReleaseTypes';
 
 export default function MovieSidebar() {
-  const { state, dispatch } = useContext(MovieFiltersContext);
-
+  const { state } = useContext(MovieFiltersContext);
   const movieGenres = useCreateMovieGenres();
-
-  const handleToggleSortSection = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        sort: { ...state.sort, expanded: !state.sort.expanded },
-      },
-    });
-  };
-
-  const handleToggleSortInput = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        sort: { ...state.sort, inputExpanded: !state.sort.inputExpanded },
-      },
-    });
-  };
-
-  const handleSort = (e: React.MouseEvent<HTMLDivElement>) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        sort: {
-          ...state.sort,
-          name: e.currentTarget.innerText,
-          value: e.currentTarget.dataset.value,
-          inputExpanded: false,
-        },
-      },
-    });
-  };
-
-  const handleToggleDates = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        dates: { ...state.dates, expanded: !state.dates.expanded },
-      },
-    });
-  };
-
-  const handleDateFrom = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        dates: {
-          ...state.dates,
-          date_from: e.target.value,
-        },
-      },
-    });
-  };
-
-  const handleDateTo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        dates: {
-          ...state.dates,
-          date_to: e.target.value,
-        },
-      },
-    });
-  };
-
-  const handleToggleGenres = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        genres: { ...state.genres, expanded: !state.genres.expanded },
-      },
-    });
-  };
-
-  const clearGenres = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: { ...state, genres: { ...state.genres, types: [] } },
-    });
-  };
-
-  const updateGenres = (id: number) => {
-    if (state?.genres?.types.includes(id)) {
-      dispatch({
-        type: 'SET_FILTERS',
-        payload: {
-          ...state,
-          genres: {
-            ...state.genres,
-            types: state.genres.types.filter((g) => g !== id),
-          },
-        },
-      });
-    } else {
-      dispatch({
-        type: 'SET_FILTERS',
-        payload: {
-          ...state,
-          genres: {
-            ...state.genres,
-            types: [...state.genres.types, id],
-          },
-        },
-      });
-    }
-  };
-
-  const handleToggleReleaseTypes = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        release_types: {
-          ...state.release_types,
-          expanded: !state.release_types.expanded,
-        },
-      },
-    });
-  };
-
-  const clearTypes = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        release_types: { ...state.release_types, types: [] },
-      },
-    });
-  };
-
-  const updateTypes = (id: number) => {
-    if (state.release_types.types.includes(id)) {
-      dispatch({
-        type: 'SET_FILTERS',
-        payload: {
-          ...state,
-          release_types: {
-            ...state.release_types,
-            types: state.release_types.types.filter((t) => t !== id),
-          },
-        },
-      });
-    } else {
-      dispatch({
-        type: 'SET_FILTERS',
-        payload: {
-          ...state,
-          release_types: {
-            ...state.release_types,
-            types: [...state.release_types.types, id],
-          },
-        },
-      });
-    }
-  };
-
-  const handleToggleAdultSection = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        adult: { ...state.adult, expanded: !state.adult.expanded },
-      },
-    });
-  };
-
-  const handleToggleRating = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        rating: { ...state.rating, expanded: !state.rating.expanded },
-      },
-    });
-  };
-
-  const handleMinRating = (value: number) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        rating: { ...state.rating, min_rating: value },
-      },
-    });
-  };
-
-  const handleMaxRating = (value: number) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        rating: { ...state.rating, max_rating: value },
-      },
-    });
-  };
-
-  const handleToggleMinimumVotes = () => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        vote_count: {
-          ...state.vote_count,
-          expanded: !state.vote_count.expanded,
-        },
-      },
-    });
-  };
-
-  const handleVoteCount = (value: number) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        vote_count: { ...state.vote_count, count: value },
-      },
-    });
-  };
-
-  const handleAdult = (e: React.MouseEvent<HTMLDivElement>) => {
-    dispatch({
-      type: 'SET_FILTERS',
-      payload: {
-        ...state,
-        adult: { ...state.adult, active: !state.adult.active },
-      },
-    });
-  };
+  const {
+    handleToggleSortSection,
+    handleToggleSortInput,
+    handleSort,
+    handleToggleDates,
+    handleDateFrom,
+    handleDateTo,
+    handleToggleGenres,
+    clearGenres,
+    updateGenres,
+    handleToggleReleaseTypes,
+    clearTypes,
+    updateTypes,
+    handleToggleAdultSection,
+    handleToggleRating,
+    handleMinRating,
+    handleMaxRating,
+    handleToggleMinimumVotes,
+    handleVoteCount,
+    handleAdult,
+  } = useMovieFilterFuntions();
 
   return (
     <aside className='sidebar'>
