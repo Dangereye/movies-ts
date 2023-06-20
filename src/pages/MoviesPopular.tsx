29a6a5dad7c8075f586691026744fe8a15382ef5
Craig Puxty <craig_puxty@talktalk.net>
2023-06-20 20:41:51 +0100

    Added page_templates folder > MoviesWithSidebar, updated MoviesPopular.

--- src/pages/MoviesPopular.tsx
+++ src/pages/MoviesPopular.tsx
@@ -5,26 +5,14 @@ import useMakeInfiniteQuery from '../hooks/useMakeInfiniteQuery';
 import useAppend from '../hooks/useAppendMovie';
 
 // Components
-import Article from '../components/articles/Article';
 import CardContent from '../components/cards/card/CardContent';
-import Container from '../components/container/Container';
 import ImageComponent from '../components/image/Image';
-import Layout from '../components/layout/Layout';
-import Main from '../components/main/Main';
-import Navigation from '../components/navigation/Navigation';
-import Sidebar from '../components/sidebar/Sidebar';
-import SubNavbar from '../components/sub_navbar/SubNavbar';
 import BodyText from '../components/typography/BodyText';
-import H2 from '../components/typography/H2';
-import Header from '../components/header/Header';
 import MobileSidebarControls from '../components/sidebar/mobile_sidebar_controls/MobileSidebarControls';
 import InfiniteCards from '../components/cards/InifinteCards';
 
 // Context
 import { MovieFiltersContext } from '../contexts/MovieFiltersContext';
-
-// Data
-import { moviePages } from '../data/moviePages';
 
 // Interfaces
 import { IPage } from '../interfaces/IPage';
@@ -32,11 +20,14 @@ import { IMovieMin } from '../interfaces/IMovieMin';
 
 // Utilities
 import { formatDate } from '../utilities/formatDate';
+import MoviesWithSidebar from '../components/page_templates/MoviesWithSidebar';
 
 export default function MoviesPopular() {
   const { state, dispatch } = useContext(MovieFiltersContext);
   const { append } = useAppend();
   const initial = useRef(false);
+  const title = 'Popular movies';
+  const name = 'popular-movies';
 
   const getNextPageParam = (page: IPage<IMovieMin>) => page.page + 1;
 
@@ -63,81 +54,51 @@ export default function MoviesPopular() {
   });
 
   if (isLoading) {
-    return <H2 heading='Loading' />;
+    return (
+      <MoviesWithSidebar title={title} name={name}>
+        <BodyText text='Loading..' />
+      </MoviesWithSidebar>
+    );
   }
 
   if (isError) {
-    return <H2 heading='Error' />;
+    return (
+      <MoviesWithSidebar title={title} name={name}>
+        <BodyText text='Oops! Something went wrong.' />
+      </MoviesWithSidebar>
+    );
   }
 
   if (movieQueries.pages[0].total_results === 0) {
     return (
-      <>
-        <SubNavbar>
-          <Navigation
-            data={moviePages}
-            getId={(item) => item.name}
-            getLink={(item) => item.link}
-            renderItem={(item) => item.name}
-            variant='horizontal'
-          />
-        </SubNavbar>
-        <Header variant='header__min' title='Popular movies' />
-        <Article name='popular-movies'>
-          <Container>
-            <Layout variant='grid grid--sidebar'>
-              <Sidebar />
-              <Main>
-                <BodyText text='No items were found that match your query.' />
-              </Main>
-            </Layout>
-          </Container>
-        </Article>
-      </>
+      <MoviesWithSidebar title={title} name={name}>
+        <BodyText text='No items were found that match your query.' />
+      </MoviesWithSidebar>
     );
   }
 
   return (
-    <>
-      <SubNavbar>
-        <Navigation
-          data={moviePages}
-          getId={(item) => item.name}
-          getLink={(item) => item.link}
-          renderItem={(item) => item.name}
-          variant='horizontal'
-        />
-      </SubNavbar>
-      <Header variant='header__min' title='Popular movies' />
-      <Article name='popular-movies'>
-        <Container>
-          <Layout variant='grid grid--sidebar'>
-            <Sidebar />
-            <Main>
-              <MobileSidebarControls />
-              <InfiniteCards
-                getId={(item) => item.id}
-                getLink={(item) => `/movies/${item.id}`}
-                renderContent={(item) => (
-                  <>
-                    <ImageComponent
-                      src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-                      fallback='/images/error_500x750.webp'
-                      alt={item.title}
-                    />
-                    <CardContent heading={item.title} vote={item.vote_average}>
-                      <BodyText text={`${formatDate(item.release_date)}`} />
-                    </CardContent>
-                  </>
-                )}
-                data={movieQueries.pages}
-                hasNextPage={hasNextPage}
-                fetchNextPage={fetchNextPage}
-              />
-            </Main>
-          </Layout>
-        </Container>
-      </Article>
-    </>
+    <MoviesWithSidebar title={title} name={name}>
+      <MobileSidebarControls />
+      <InfiniteCards
+        getId={(item) => item.id}
+        getLink={(item) => `/movies/${item.id}`}
+        renderContent={(item) => (
+          <>
+            <ImageComponent
+              src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+              fallback='/images/error_500x750.webp'
+              alt={item.title}
+            />
+            <CardContent heading={item.title} vote={item.vote_average}>
+              <BodyText text={`${formatDate(item.release_date)}`} />
+            </CardContent>
+          </>
+        )}
+        data={movieQueries.pages}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+      />
+    </MoviesWithSidebar>
   );
 }
