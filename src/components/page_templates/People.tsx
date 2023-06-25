008e927761149aaa1207f2563b24b27ed9861077
Craig Puxty <craig_puxty@talktalk.net>
2023-06-25 21:03:32 +0100

    Added People page template, updated PeoplePopular.

---
+++ src/components/page_templates/People.tsx
@@ -0,0 +1,40 @@
+// Data
+import { peoplePages } from '../../data/peoplePages';
+
+// Components
+import Article from '../articles/Article';
+import Container from '../container/Container';
+import Header from '../header/Header';
+import Main from '../main/Main';
+import Navigation from '../navigation/Navigation';
+import SubNavbar from '../sub_navbar/SubNavbar';
+
+type PeopleProps = {
+  title: string;
+  name: string;
+  children: React.ReactNode;
+};
+
+export default function People({ title, name, children }: PeopleProps) {
+  return (
+    <>
+      <SubNavbar>
+        <Navigation
+          data={peoplePages}
+          getId={(item) => item.name}
+          getLink={(item) => item.link}
+          renderItem={(item) => item.name}
+          variant='horizontal'
+        />
+      </SubNavbar>
+      <Header variant='header__min' title='Popular people' />
+      <Article name='popular-people'>
+        <Container>
+          <Main>
+            <>{children}</>
+          </Main>
+        </Container>
+      </Article>
+    </>
+  );
+}
--- src/pages/PeoplePopular.tsx
+++ src/pages/PeoplePopular.tsx
@@ -1,39 +1,27 @@
+// Interfaces
 import { IPage } from '../interfaces/IPage';
 import { IPerson } from '../interfaces/IPerson';
-import { useEffect, useContext } from 'react';
 
 // Hooks
 import useMakeInfiniteQuery from '../hooks/useMakeInfiniteQuery';
-import useAppend from '../hooks/useAppendMovie';
 
 // Components
-import Article from '../components/articles/Article';
 import CardContent from '../components/cards/card/CardContent';
-import Container from '../components/container/Container';
 import ImageComponent from '../components/image/Image';
-import Layout from '../components/layout/Layout';
-import Main from '../components/main/Main';
-import Navigation from '../components/navigation/Navigation';
-import Sidebar from '../components/sidebar/Sidebar';
-import SubNavbar from '../components/sub_navbar/SubNavbar';
 import BodyText from '../components/typography/BodyText';
-import H2 from '../components/typography/H2';
-import Header from '../components/header/Header';
-import MobileSidebarControls from '../components/sidebar/mobile_sidebar_controls/MobileSidebarControls';
 import InfiniteCards from '../components/cards/InifinteCards';
-
-// Utilities
-import { formatDate } from '../utilities/formatDate';
-import { peoplePages } from '../data/peoplePages';
+import Loader from '../components/loader/Loader';
+import People from '../components/page_templates/People';
 
 export default function PeoplePopular() {
   const getNextPageParam = (page: IPage<IPerson>) => page.page + 1;
+  const title = 'People: popular';
+  const name = 'people-popular';
 
   const {
     data: movieQueries,
     isError,
     isLoading,
-    refetch,
     hasNextPage,
     fetchNextPage,
   } = useMakeInfiniteQuery<IPage<IPerson>>(
@@ -43,48 +31,42 @@ export default function PeoplePopular() {
   );
 
   if (isLoading) {
-    return <H2 heading='Loading' />;
+    return (
+      <People title={title} name={name}>
+        <Loader />
+      </People>
+    );
   }
 
   if (isError) {
-    return <H2 heading='Error' />;
+    return (
+      <People title={title} name={name}>
+        <BodyText text='Oops! Something went wrong.' />
+      </People>
+    );
   }
 
   return (
-    <>
-      <SubNavbar>
-        <Navigation
-          data={peoplePages}
-          getId={(item) => item.name}
-          getLink={(item) => item.link}
-          renderItem={(item) => item.name}
-          variant='horizontal'
-        />
-      </SubNavbar>
-      <Header variant='header__min' title='Popular people' />
-      <Article name='popular-people'>
-        <Container>
-          <InfiniteCards
-            getId={(item) => item.id}
-            getLink={(item) => `/people/${item.id}`}
-            renderContent={(item) => (
-              <>
-                <ImageComponent
-                  src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`}
-                  fallback='/images/error_500x750.webp'
-                  alt={item.name}
-                />
-                <CardContent heading={item.name}>
-                  <BodyText text={item.known_for_department} />
-                </CardContent>
-              </>
-            )}
-            data={movieQueries.pages}
-            hasNextPage={hasNextPage}
-            fetchNextPage={fetchNextPage}
-          />
-        </Container>
-      </Article>
-    </>
+    <People title={title} name={name}>
+      <InfiniteCards
+        getId={(item) => item.id}
+        getLink={(item) => `/people/${item.id}`}
+        renderContent={(item) => (
+          <>
+            <ImageComponent
+              src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`}
+              fallback='/images/error_500x750.webp'
+              alt={item.name}
+            />
+            <CardContent heading={item.name}>
+              <BodyText text={item.known_for_department} />
+            </CardContent>
+          </>
+        )}
+        data={movieQueries.pages}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+      />
+    </People>
   );
 }
