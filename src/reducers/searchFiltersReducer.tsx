d8ade1f9fb5593494086b8006ddf66338b8816d8
Craig Puxty <craig_puxty@talktalk.net>
2023-06-28 12:06:57 +0100

    Added searchFiltersReducer.

---
+++ src/reducers/searchFiltersReducer.tsx
@@ -0,0 +1,30 @@
+export type stateType = {
+  show_media_type: 'movies' | 'tv-shows' | 'people';
+};
+
+export const InitialState: stateType = {
+  show_media_type: 'movies',
+};
+
+export type actionType = {
+  type: string;
+  payload: stateType;
+};
+
+export default function searchFiltersReducer(
+  state: stateType,
+  action: actionType
+) {
+  const { type, payload } = action;
+  switch (type) {
+    case 'SET_FILTERS': {
+      return {
+        ...state,
+        ...payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
