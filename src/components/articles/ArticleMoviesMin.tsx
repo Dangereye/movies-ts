bb0d2a5794e3be7adfd6042f5e698f85de0f157d
Craig Puxty <craig_puxty@talktalk.net>
2023-07-31 21:17:33 +0100

    Updated ArticleMoviesMin image logic.

--- src/components/articles/ArticleMoviesMin.tsx
+++ src/components/articles/ArticleMoviesMin.tsx
@@ -42,7 +42,11 @@ export default function ArticleMoviesMin({
             renderContent={(item) => (
               <>
                 <ImageComponent
-                  src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                  src={
+                    item.poster_path
+                      ? `https://image.tmdb.org/t/p/w500/${item.poster_path}`
+                      : 'error_500x750.webp'
+                  }
                   fallback='/images/error_500x750.webp'
                   alt={item.title}
                 />
