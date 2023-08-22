9232f5118664082344bce0aee4dc4e4fed89c55f
Craig Puxty <craig_puxty@talktalk.net>
2023-08-22 21:37:22 +0100

    Updated ArticleImagesScrollX.

--- src/components/articles/ArticleImagesScrollX.tsx
+++ src/components/articles/ArticleImagesScrollX.tsx
@@ -4,6 +4,9 @@ import { useContext } from 'react';
 // React router
 import { Link, useLocation } from 'react-router-dom';
 
+// Hooks
+import useCreateLanguages from '../../hooks/useCreateLanguages';
+
 // Contexts
 import { ImagesFiltersContext } from '../../contexts/ImagesFiltersContext';
 
@@ -19,6 +22,9 @@ import ImageComponent from '../image/Image';
 export default function ArticleImages() {
   const { state, dispatch } = useContext(ImagesFiltersContext);
   const { pathname } = useLocation();
+
+  const languages = useCreateLanguages();
+
   const images =
     state.languages[state.display.show_media_type][
       state.languages.active_language
@@ -41,106 +47,124 @@ export default function ArticleImages() {
     });
   };
 
-  if (images && images?.length > 1) {
+  const getLanguageName = (iso_639_1: string) => {
+    const language = languages.find((lang) => lang.iso_639_1 === iso_639_1);
+    if (language?.english_name) {
+      return language?.english_name;
+    }
+    return 'international';
+  };
+
+  if (
+    state.display.results.posters > 0 ||
+    state.display.results.backdrops > 0
+  ) {
     return (
       <Article name='images'>
         <Container>
           <H2 heading={state?.display?.show_media_type} />
           <Wrapper name='image options' variant='flex'>
-            {state?.display?.results?.posters > 0 && (
-              <Button
-                name={
-                  <>
-                    <span className='name'>Posters</span>
-                    <span className='qty'>
-                      {state?.display?.results?.posters}
-                    </span>
-                  </>
-                }
-                active={state?.display?.show_media_type === 'posters'}
-                variant='btn--tertiary'
-                onClick={() => updateImages('posters')}
-              />
-            )}
-            {state.display.results.backdrops > 0 && (
-              <Button
-                name={
-                  <>
-                    <span className='name'>Backdrops</span>
-                    <span className='qty'>
-                      {state?.display?.results?.backdrops}
-                    </span>
-                  </>
-                }
-                active={state?.display?.show_media_type === 'backdrops'}
-                variant='btn--tertiary'
-                onClick={() => updateImages('backdrops')}
-              />
-            )}
+            <Button
+              name={
+                <>
+                  <span className='name'>Posters</span>
+                  <span className='qty'>
+                    {state?.display?.results?.posters}
+                  </span>
+                </>
+              }
+              active={state?.display?.show_media_type === 'posters'}
+              variant='btn--tertiary'
+              onClick={() => updateImages('posters')}
+            />
+
+            <Button
+              name={
+                <>
+                  <span className='name'>Backdrops</span>
+                  <span className='qty'>
+                    {state?.display?.results?.backdrops}
+                  </span>
+                </>
+              }
+              active={state?.display?.show_media_type === 'backdrops'}
+              variant='btn--tertiary'
+              onClick={() => updateImages('backdrops')}
+            />
           </Wrapper>
           <BodyText
-            text={`Showing ${
-              images?.length > 10
-                ? '10'
-                : images?.length < 10
-                ? images?.length
-                : 0
-            } ${state?.display?.show_media_type}`}
+            text={
+              images
+                ? `Showing ${
+                    images?.length > 10 ? '10' : images.length
+                  } ${getLanguageName(state.languages.active_language)} ${
+                    state?.display?.show_media_type
+                  }`
+                : null
+            }
           />
           <div className='images__scroll'>
-            {images
-              ?.filter((image, i) => i < 10)
-              .map((image, i) => (
-                <div key={image.file_path} onClick={() => openModal(i)}>
-                  <ImageComponent
-                    key={image.file_path}
-                    base_url='https://image.tmdb.org/t/p/'
-                    filename={image.file_path}
-                    poster_sizes={
-                      state.display.show_media_type === 'posters' ? 'w300' : ''
-                    }
-                    backdrop_sizes={
-                      state.display.show_media_type === 'backdrops'
-                        ? 'w780'
-                        : ''
-                    }
-                    fallback={
-                      state.display.show_media_type === 'posters'
-                        ? '/images/error_300x450.webp'
-                        : '/images/error_500x281.webp'
-                    }
-                    width={
-                      state.display.show_media_type === 'posters' ? 300 : 500
-                    }
-                    aspect_ratio={
-                      state.display.show_media_type === 'posters'
-                        ? 'aspect-ratio-2-3'
-                        : 'aspect-ratio-16-9'
-                    }
-                    alt={`${state?.display?.show_media_type}-${i}`}
-                    loading='lazy'
-                  />
-                </div>
-              ))}
+            {images ? (
+              images
+                ?.filter((image, i) => i < 10)
+                .map((image, i) => (
+                  <div key={image.file_path} onClick={() => openModal(i)}>
+                    <ImageComponent
+                      key={image.file_path}
+                      base_url='https://image.tmdb.org/t/p/'
+                      filename={image.file_path}
+                      poster_sizes={
+                        state.display.show_media_type === 'posters'
+                          ? 'w300'
+                          : ''
+                      }
+                      backdrop_sizes={
+                        state.display.show_media_type === 'backdrops'
+                          ? 'w780'
+                          : ''
+                      }
+                      fallback={
+                        state.display.show_media_type === 'posters'
+                          ? '/images/error_300x450.webp'
+                          : '/images/error_500x281.webp'
+                      }
+                      width={
+                        state.display.show_media_type === 'posters' ? 300 : 500
+                      }
+                      aspect_ratio={
+                        state.display.show_media_type === 'posters'
+                          ? 'aspect-ratio-2-3'
+                          : 'aspect-ratio-16-9'
+                      }
+                      alt={`${state?.display?.show_media_type}-${i}`}
+                      loading='lazy'
+                    />
+                  </div>
+                ))
+            ) : (
+              <BodyText
+                text={`No ${getLanguageName(state.languages.active_language)} ${
+                  state.display.show_media_type
+                } available.`}
+              />
+            )}
           </div>
-          {(state?.display?.results?.posters > 10 ||
-            state?.display?.results?.backdrops > 10) && (
-            <div className='buttons'>
-              <Link
-                to={
-                  pathname.includes('movie')
-                    ? `/movies/${state.id}/images`
-                    : `/tv/${state.id}/images`
-                }
-                className='btn btn--tertiary'
-              >
-                view all images
-              </Link>
-            </div>
-          )}
+          <div className='buttons'>
+            <Link
+              to={
+                pathname.includes('movie')
+                  ? `/movies/${state.id}/images`
+                  : `/tv/${state.id}/images`
+              }
+              className='btn btn--tertiary'
+            >
+              view all images
+            </Link>
+          </div>
         </Container>
       </Article>
     );
   }
+
   return null;
 }
